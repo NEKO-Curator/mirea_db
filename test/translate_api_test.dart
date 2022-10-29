a6391bacf576193ebba8835393d3bbac9ecc3b84
NEKO-Curator <xiao.akrona@gmail.com>
2022-10-29 19:56:50 +0300

    test update

--- test/translate_api_test.dart
+++ test/translate_api_test.dart
@@ -1,37 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart' as http;
-import 'package:mirea_db/test_api/album_model.dart';
-import 'package:mirea_db/test_api/special_api_for_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mirea_db/api/translate_api.dart';
 
-import 'translate_api_test.mocks.dart';
-
-@GenerateMocks([http.Client])
 void main() {
-  group('fetchAlbum', () {
-    test('Удачный ответ', () async {
-      final client = MockClient();
-
-      //используем мокито для возврата удачного ответа
-      // provided http.Client.
-      when(client
-              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
-          .thenAnswer((_) async =>
-              http.Response('{"userId": 1, "id": 2, "title": "test"}', 200));
-
-      expect(await fetchAlbum(client), isA<Album>());
+  group('Тестирование апи метода возвращающего перевод', () {
+    test('При пустом запросе возвращает пустой ответ', () async {
+      final testResponse = await fetchTranslate('');
+      expect(testResponse, '');
     });
 
-    test('Неудачный ответ бросит исключение', () {
-      final client = MockClient();
+    test('При не пустом запросе должен возвращать перевод с китайского',
+        () async {
+      final testResponse = await fetchTranslate('test');
+      expect(testResponse, '测试');
+    });
 
-      // используем мокито чтобы вызвать неудачный ответ
-      when(client
-              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
-          .thenAnswer((_) async => http.Response('Not Found', 404));
-
-      expect(fetchAlbum(client), throwsException);
+    test(
+        'Даже при отсутсвии букв должен отправлять запрос и возвращать значение',
+        () async {
+      final testResponse = await fetchTranslate('1234+');
+      expect(testResponse, '1234+');
     });
   });
 }
